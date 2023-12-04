a2206fc786cd7424d8238f5c4076a139aa513687
lovasoa3 <hei.lovasoa.3@gmail.com>
2023-12-04 11:52:28 +0100

    projet poja-prime

---
+++ poja-prime/src/main/java/com/example/pojaprime/PojaPrimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pojaprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojaPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojaPrimeApplication.class, args);
+	}
+
+}
---
+++ poja-prime/src/test/java/com/example/pojaprime/PojaPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pojaprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojaPrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
